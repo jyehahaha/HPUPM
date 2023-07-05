d9210b65123060ae3e590467d9845f9787f7276d
jyehahaha <nazihaizzati@gmail.com>
2023-07-05 11:36:13 +0800

    code commit

--- HPUPM/settings.py
+++ HPUPM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
